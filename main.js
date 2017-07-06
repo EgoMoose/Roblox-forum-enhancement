30bf56fbd95777e6a8c28ef472a95bcfda8fba0b
EgoMoose <EgoMoose@users.noreply.github.com>
2017-07-06 12:35:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
-var elemt = document.getElementById("ctl00_cphRoblox_PostView1_ctl00_PostList");
-elemt.innerHTML = elemt.innerHTML.split("<br>").join("\n");
+var forum = document.getElementById("ctl00_cphRoblox_PostView1_ctl00_PostList");
+var preview = document.getElementById("ctl00_cphRoblox_Createeditpost1_PostForm_PreviewBody");
 
-renderCodeInElement(elemt);
-renderMathInElement(elemt, {
-	delimiters: [
-		{left: "$$", right: "$$", display: false},
-		{left: "\\(", right: "\\)", display: false},
-		{left: "\\[", right: "\\]", display: true}
-	]
-});
+if (forum) {
+	/*
+	var posts = document.getElementsByClassName("normalTextSmall notranslate linkify");
+	for (var i = 0; i < posts.length; i++) {
+		var pre = document.createElement("PRE");
+		pre.innerHTML = posts[i].innerHTML.split("<br>").join("\n");
+		posts[i].innerHTML = "";
+		posts[i].appendChild(pre);
+		
+		renderCodeInElement(pre);
+		renderMathInElement(pre, {
+			delimiters: [
+				{left: "$$", right: "$$", display: false},
+				{left: "\\(", right: "\\)", display: false},
+				{left: "\\[", right: "\\]", display: true}
+			]
+		});
+	}
+	*/
+	
+	forum.innerHTML = forum.innerHTML.split("<br>").join("\n");
+	renderCodeInElement(forum);
+	renderMathInElement(forum, {
+		delimiters: [
+			{left: "$$", right: "$$", display: false},
+			{left: "\\(", right: "\\)", display: false},
+			{left: "\\[", right: "\\]", display: true}
+		]
+	});
+}
+
+
+if (preview) {
+	var pre = document.createElement("PRE");
+	pre.innerHTML = preview.innerHTML.split("<br>").join("\n");
+	preview.innerHTML = "";
+	preview.appendChild(pre); 
+
+	renderCodeInElement(pre);
+	renderMathInElement(pre, {
+		delimiters: [
+			{left: "$$", right: "$$", display: false},
+			{left: "\\(", right: "\\)", display: false},
+			{left: "\\[", right: "\\]", display: true}
+		]
+	});
+}
